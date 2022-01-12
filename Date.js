87920cfdbf2adfb4b78cea512e8b00e95155d110
Austin Kelleher <austinlkelleher@gmail.com>
2022-01-11 21:37:41 -0500

    Fixes #14 - Handle sub-millisecond date precision in coercion
    Leverage the native `Date.parse` function to handle `Date`
    coercion when the source type is a string. This handles the
    case when the supplied string was generated with sub-millisecond
    precision.

--- Date.js
+++ Date.js
@@ -1,5 +1,7 @@
-const isoDateFormat =
-    /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d+))?(Z)?$/;
+function coerceDateString (value) {
+  const parsed = Date.parse(value);
+  return isNaN(parsed) ? null : new Date(parsed);
+}
 
 module.exports = require('./Model').extend({
   typeName: 'date',
@@ -15,21 +17,7 @@ module.exports = require('./Model').extend({
 
     if (value != null) {
       if (value.constructor === String) {
-        const a = isoDateFormat.exec(value);
-        if (a) {
-          let millisecond = a[7];
-          millisecond = (millisecond === undefined) ? 0 : +millisecond;
-
-          if (a[8] === 'Z') {
-            // The `Z` suffix is used to indicate UTC time zone
-            value = new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4], +a[5], +a[6], millisecond));
-          } else {
-            // No `Z` suffix so implicitly use time zone of this computer
-            value = new Date(+a[1], +a[2] - 1, +a[3], +a[4], +a[5], +a[6], millisecond);
-          }
-        } else {
-          return null;
-        }
+        return coerceDateString(value);
       } else if (value.constructor === Number) {
         value = new Date(value);
       } else if (value.constructor === Date) {
